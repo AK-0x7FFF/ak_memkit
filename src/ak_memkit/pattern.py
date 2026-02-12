d14435352af1aa207760661e81f9e7d963ce3711
AK-0x7FFF <akpainkiller32767@gmail.com>
2026-02-13 00:10:55 +0800

    Removed Pattern "module" argument and .address() .to_address() .update_module_base()

--- src/ak_memkit/pattern.py
+++ src/ak_memkit/pattern.py
@@ -1,5 +1,5 @@
 from re import search, DOTALL
-from typing import Self
+from typing import Self, Any
 
 from .memory.process import Process
 from .memory.abstract_classes import MemoryReadAbs, ModuleAbs
@@ -7,26 +7,21 @@ from .address import Address
 
 
 class Pattern:
-    def __init__(self, pattern: str, module: ModuleAbs, module_buffer: bytes | None = None):
+    def __init__(self, pattern: str, buffer: bytes | bytearray | memoryview | None = None):
         self.pattern = pattern
-
-        self.module = module
-        self._module_base = self.module.base
-        self._module_buffer = module_buffer
-        if self._module_buffer is None:
-            self._module_buffer = Process.get().memory_read.read_memory(self.module.base, self.module.size)
+        self.buffer = buffer
 
         self._pattern_offset: int | None = None
 
     def __repr__(self) -> str:
-        return "Pattern(\"%s\", %s)" % (self.pattern, hex(self.address))
+        return "Pattern(\"%s\", %s)" % (self.pattern, self.offset)
 
-    @property
-    def address(self) -> int:
-        return self._module_base + self._pattern_offset
-
-    def to_address(self) -> Address:
-        return Address(self._module_base + self._pattern_offset)
+    # @property
+    # def address(self) -> int:
+    #     return self._module_base + self._pattern_offset
+    #
+    # def to_address(self) -> Address:
+    #     return Address(self._module_base + self._pattern_offset)
 
     @property
     def offset(self) -> int:
@@ -43,7 +38,7 @@ class Pattern:
         try:
             if auto_trans_2_regex: pattern_match_bytes = self.pattern_str_to_regex_bytes(self.pattern)
             else: pattern_match_bytes = self.pattern
-            match_offset = search(pattern_match_bytes, self._module_buffer, DOTALL)
+            match_offset = search(pattern_match_bytes, self.buffer, DOTALL)
 
             if match_offset is None:
                 # raise PatternConvertError(self.pattern, pattern_match_bytes)
@@ -55,8 +50,8 @@ class Pattern:
             raise RuntimeError() from err
 
 
-    def pattern_bytes(self, offset: int, size: int) -> bytes:
-        return self._module_buffer[self._pattern_offset + offset:self._pattern_offset + offset + size]
+    def pattern_bytes(self, offset: int, size: int) -> bytes | bytearray | memoryview:
+        return self.buffer[self._pattern_offset + offset:self._pattern_offset + offset + size]
 
 
     def add(self, value: int) -> Self:
@@ -73,6 +68,6 @@ class Pattern:
 
         return self
 
-    def update_module_base(self, module_base: int) -> Self:
-        self._module_base = module_base
-        return self
+    # def update_module_base(self, module_base: int) -> Self:
+    #     self._module_base = module_base
+    #     return self
